e2772ad976baeea5b4730dcf4499f60fb6db7875
0x2b375 <giiguu.giigu@gmail.com>
2025-03-13 04:51:09 +0800

    added sheet for mobile nav menu

--- src/components/Dashboard/navigation.tsx
+++ src/components/Dashboard/navigation.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Menu } from "lucide-react";
+import { useState } from "react";
+import { useLocation, useNavigate } from "react-router";
+import { useMedia } from "react-use";
 import { NavButton } from "../nav-button";
 
 const routes = [
@@ -16,6 +22,46 @@ const routes = [
 ];
 
 export const Navigation = () => {
+  const navigate = useNavigate();
+  const pathname = useLocation();
+  const [isOpen, setIsOpen] = useState(false);
+  const isMobile = useMedia("(max-width: 1024px)", false);
+
+  const onClick = (href: string) => {
+    navigate(href);
+    setIsOpen(false); // Close the sheet after navigation
+  };
+
+  if (isMobile) {
+    return (
+      <Sheet open={isOpen} onOpenChange={setIsOpen}>
+        <SheetTrigger>
+          <Button
+            variant="outline"
+            size="sm"
+            className="font-normal bg-white/10 hover:bg-white/20 hover:text-white border-none focus-visible:ring-offset-0 focus-visible:ring-transparent focus:bg-white/30 transition"
+          >
+            <Menu className="size-4" />
+          </Button>
+        </SheetTrigger>
+        <SheetContent className="px-2" side="left">
+          <nav className="flex flex-col gap-y-2 pt-10">
+            {routes.map((route) => (
+              <Button
+                variant={route.href === pathname.pathname ? "default" : "ghost"}
+                key={route.href}
+                onClick={() => onClick(route.href)}
+                className="w-full justify-start"
+              >
+                {route.label}
+              </Button>
+            ))}
+          </nav>
+        </SheetContent>
+      </Sheet>
+    );
+  }
+
   return (
     <nav className="hidden lg:flex items-center gap-x-2 overflow-x-auto">
       {routes.map((route) => (
