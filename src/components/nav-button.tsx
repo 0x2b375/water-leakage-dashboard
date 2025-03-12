183997035babfff6c0b96bf5b636c936e6fa2fbc
0x2b375 <giiguu.giigu@gmail.com>
2025-03-13 04:32:36 +0800

    added nav button

--- src/components/nav-button.tsx
+++ src/components/nav-button.tsx
@@ -1,4 +1,5 @@
-import { Button, ConfigProvider } from "antd";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import { NavLink } from "react-router";
 
 type Props = {
@@ -8,28 +9,19 @@ type Props = {
 
 export const NavButton = ({ href, label }: Props) => {
   return (
-    <ConfigProvider
-      theme={{
-        components: {
-          Button: {
-            ghostBg: "transparent",
-          },
-        },
-      }}
-    >
-      <NavLink to={href}>
-        {({ isActive }) => (
-          <Button
-            type={isActive ? "primary" : "text"}
-            size="small"
-            className={`w-full !p-4 lg:w-auto justify-between font-normal hover:bg-white/50 hover:text-white focus-visible:ring-offset-0 focus-visible:ring-transparent !text-white outline-none transition-all duration-300 ${
-              isActive ? "bg-white/10 text-white" : "bg-transparent"
-            }`}
-          >
-            {label}
-          </Button>
-        )}
-      </NavLink>
-    </ConfigProvider>
+    <NavLink to={href}>
+      {({ isActive }) => (
+        <Button
+          size="sm"
+          variant="outline"
+          className={cn(
+            "w-full !p-4 lg:w-auto justify-between font-normal hover:bg-white/25 cursor-pointer hover:text-white border-none focus-visible:ring-offset-0 focus-visible:ring-transparent text-white outline-none transition-all duration-300",
+            isActive ? "bg-white/25 text-white" : "bg-transparent"
+          )}
+        >
+          {label}
+        </Button>
+      )}
+    </NavLink>
   );
 };
