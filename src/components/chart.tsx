62c5f0e510da491f1dd36330fbefc62d9b98a1fd
0x2b375 <giiguu.giigu@gmail.com>
2025-03-13 08:40:54 +0800

    added loader for charts

--- src/components/chart.tsx
+++ src/components/chart.tsx
@@ -1,11 +1,12 @@
 import { SelectValue } from "@radix-ui/react-select";
-import { AreaChart, BarChart3, FileSearch, LineChart } from "lucide-react";
+import { AreaChart, BarChart3, FileSearch, LineChart, Loader2 } from "lucide-react";
 import { useState } from "react";
 import { AreaVariant } from "./Charts/aria-variant";
 import { BarVariant } from "./Charts/bar-variant";
 import { LineVariant } from "./Charts/line-variant";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 import { Select, SelectContent, SelectItem, SelectTrigger } from "./ui/select";
+import { Skeleton } from "./ui/skeleton";
 
 type Props = {
   data?: {
@@ -66,6 +67,22 @@ export const Chart = ({ data = [] }: Props) => {
             {chartType === "bar" && <BarVariant data={data} />}
           </>
         )}
+      </CardContent>
+    </Card>
+  );
+};
+
+export const ChartLoading = () => {
+  return (
+    <Card className="border-none drop-shadow-sm">
+      <CardHeader className="flex space-y-2 lg:space-y-0 lg:flex-row lg:items-center justify-between">
+        <Skeleton className="h-8 w-48" />
+        <Skeleton className="h-8 lg:w-[120px] w-full" />
+      </CardHeader>
+      <CardContent>
+        <div className="h-[350px] w-full flex items-center justify-center">
+          <Loader2 className="h-6 w-6 text-slate-300 animate-spin" />
+        </div>
       </CardContent>
     </Card>
   );
