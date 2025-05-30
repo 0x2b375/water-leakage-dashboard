df2905966946b1ee698cf6220e940c9091ca0347
0xb39 <giiguu.giigu@gmail.com>
2025-05-30 23:05:56 +0800

    reverted back to local

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,13 @@
 import { Pool } from "pg";
+import dotenv from "dotenv";
 
+dotenv.config();
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }, 
-});
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: Number(process.env.PGPORT),
+})
 
 export default pool;
--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://water-leakage-dashboard-pxvf.vercel.app/api";
+const API_BASE_URL = "http://localhost:3000/api";
 
 export async function fetchSensorData() {
   try {
